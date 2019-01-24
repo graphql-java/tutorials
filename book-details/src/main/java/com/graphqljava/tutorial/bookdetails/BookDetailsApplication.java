c23f32d82a39420cd4d9cd6c244bfbc839f7e146
Andreas Marek <andimarek@fastmail.fm>
2019-01-24 13:28:31 +1100

    book-details example

---
+++ book-details/src/main/java/com/graphqljava/tutorial/bookdetails/BookDetailsApplication.java
@@ -0,0 +1,14 @@
+package com.graphqljava.tutorial.bookdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDetailsApplication.class, args);
+    }
+
+}
+
